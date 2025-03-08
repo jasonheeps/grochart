10c711db450c9983d4dbccf3a8ab3c8d4938340b
jasonheeps <pschaefer@posteo.net>
2025-03-08 19:31:36 +0100

    new + params CirclesController

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,2 +1,11 @@
 class CirclesController < ApplicationController
+  def new
+    # TODO: remove the print
+    print "def 'new' in CirclesController called!"
+    @circle = Circle.new
+  end
+
+  def organization_params
+    params.require(:organization).permit(:name, :acronym, :parent_id)
+  end
 end
